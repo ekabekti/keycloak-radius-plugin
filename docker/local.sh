f40d850af8715293a9514102535508fa625693f3
ekabekti <newhanoman@gmail.com>
2023-09-12 23:42:48 +0700

    modified:   docker/local.sh

--- docker/local.sh
+++ docker/local.sh
@@ -1,5 +1,5 @@
 docker stop keycloak-radius-plugin
 docker rm keycloak-radius-plugin
 docker build -t keycloak-radius-plugin .
-docker run --env-file .example.radius.env -e KEYCLOAK_ADMIN_PASSWORD="admin" -e KEYCLOAK_ADMIN="admin"  -p 8080:8080/tcp -p 1812:1812/udp -p 1813:1813/udp --name=keycloak-radius-plugin keycloak-radius-plugin start
+docker run --env-file .example.radius.env -p 8080:8080/tcp -p 1812:1812/udp -p 1813:1813/udp --name=keycloak-radius-plugin keycloak-radius-plugin start-dev
 set -e
